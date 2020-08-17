d42b52c762964f343940c617735d5ca33ea486f4
Pierre Deveix <deveixpierre@gmail.com>
2020-08-17 17:06:14 +0200

    problem solved in superpowers controller

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,5 +1,5 @@
 class SuperpowersController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index, :show
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
   end
