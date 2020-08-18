76478efb0e884b3c513f6cc8c16d15f8729e36f9
Amelie Pol <pol.amelie@gmail.com>
2020-08-18 17:58:32 +0200

    Back du controller Rentings

--- app/controllers/rentings_controller.rb
+++ app/controllers/rentings_controller.rb
@@ -1,21 +1,49 @@
 class RentingsController < ApplicationController
+
   def index
+    @rentings = Renting.all
+  end
+
+  def show
+    @superpower = Superpower.find(params[:id])
+    @renting = Renting.new
+  end
+
+  def new
+    @superpower = Superpower.find(params[:superpower_id])
+    @renting = Renting.new
   end
 
   def create
+    @renting = Renting.new(rentings_params)
+    @superpower = Superpower.find(params[:superpower_id])
+    @renting.superpower = @superpower
+      if @renting.save
+        redirect_to superpower_path(@superpower)
+      else
+        render :new
+      end
   end
 
   def edit
+    @renting = Renting.find(params[:id])
   end
 
   def update
+    @renting = Renting.find(params[:id])
+    @renting.update(rentings_params)
+    redirect_to superpower_path(@superpower)
   end
 
   def destroy
+    @renting = Renting.find(params[:id])
+    @renting.destroy
+    redirect_to superpower_path(@renting.superpower)
   end
 
   private
 
   def rentings_params
+    params.require(:renting).permit(:start_date, :end_date)
   end
 end
--- app/models/renting.rb
+++ app/models/renting.rb
@@ -1,4 +1,15 @@
 class Renting < ApplicationRecord
   belongs_to :user
   belongs_to :superpower
+
+  def renting_duration
+    raise
+    renting = Renting.find(params[:id])
+    (renting.end_date - renting.start_date).to_i
+    redirect_to ...
+  end
+
+  def total_price
+    @renting_duration * :fee
+  end
 end
