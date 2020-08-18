d51bd732506c14707e12b9c38125de7c7e558a65
Pierre Deveix <deveixpierre@gmail.com>
2020-08-18 11:26:12 +0200

    index show init

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,10 +1,17 @@
 class SuperpowersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_superpower, only: [:show]
 
   def index
-    @superpower = Superpower.all
+    @superpowers = Superpower.all
   end
 
   def show
+  end
+
+  private
+
+  def set_superpower
+    @superpower = Superpower.find(params[:id])
   end
 end
