b240c784a0614d109cb68f60518ca9641562fecd
Pierre Deveix <deveixpierre@gmail.com>
2020-08-19 12:41:51 +0200

    background images on home done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
-  @cat = [['Daily Life', 'cat_dailylife.jpg'], ['Arts & Culture', 'cat_arts_and_culture.jpg'], 'Animals & Creatures', 'Transportation', 'Almost legal']
-
- def home
+  def home
     @categories = Category.all
   end
 end
