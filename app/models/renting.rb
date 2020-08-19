86343cd60566eab8fa0ede177daea0d515b68ff8
Amelie Pol <pol.amelie@gmail.com>
2020-08-19 15:41:49 +0200

    Rentings front ok for today

--- app/models/renting.rb
+++ app/models/renting.rb
@@ -3,6 +3,6 @@ class Renting < ApplicationRecord
   belongs_to :superpower
 
   def set_total_price(fee)
-    self.total_price = (end_date - start_date).to_i * fee
+    update(total_price: (end_date - start_date).to_i * fee)
   end
 end
