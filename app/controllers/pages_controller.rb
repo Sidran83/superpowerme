c0bc05d238ca1ebe4bca5fc4023cbbfda1789cd5
Pierre Deveix <deveixpierre@gmail.com>
2020-08-19 11:38:32 +0200

    buttons home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-    skip_before_action :authenticate_user!, only: :home
-  def home
+  skip_before_action :authenticate_user!, only: :home
+
+  @cat = [['Daily Life', 'cat_dailylife.jpg'], ['Arts & Culture', 'cat_arts_and_culture.jpg'], 'Animals & Creatures', 'Transportation', 'Almost legal']
+
+ def home
     @categories = Category.all
   end
 end
